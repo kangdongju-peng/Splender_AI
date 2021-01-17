9b4a1042d70fe4518e03f851fde8b2acbfde55d3
kangdongju-peng <kangdongju05@gmail.com>
2021-01-17 22:43:57 +0900

    뼈대 만들기

--- DQN.py
+++ DQN.py
@@ -7,6 +7,8 @@ from collections import deque
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.models import Sequential
+from enviornment import Env, GraphicDisplay
+import gym
 
 EPISODES = 300
 
@@ -17,6 +19,7 @@ class DQNAgent:
         self.epsilon = 1.0 # e-탐욕 정책사용, 탐험률 일단 1.0 설정
         self.epsilon_min = 0.01 #탐험률의 최소 값 정의
         self.epsilon_decay = 0.999 # 탐험률의 감소값 일단 0.999로 설정
+        self.train_start = 1000
 
         self.state_size = state_size
         self.action_size = action_size
@@ -85,12 +88,35 @@ class DQNAgent:
 
 if __name__ == "__main__":
 
+    env = Env # 환경 가저오기!
+    state_size = env.state_space # <-- 이함수 없어서 오류남 환경에 상태의 크기를 가져오는 함수가 필요함
+    action_size = env.action_space # <-- 이함수 없어서 오류남 환경에 액션의 크기를 가져오는 함수가 필여
+    agent = DQNAgent(state_size, action_size) #이제부터 환경의 함수가 필요한데.,,, 내일 말조 ㅁ해보고 하자
 
-    ##agent = DQNAgent(state_size, action_size) 이제부터 환경의 함수가 필요한데.,,, 내일 말조 ㅁ해보고 하자
+    scores, episodes = [],[]
 
 
     for e in range(EPISODES):
         done = False
+        score = 0
+        state = env.reset() # <-- 지금 함수 없어서 오류 상태 리셋하는 함수 env에 넣어야함
+        state = env.Startset() # <-- 함수없어서 오류남 시작할때 상태를 나타내는거
+
+        while not done:
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)  # < -- 없어서 오류남 env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
+            #next_state = np.reshape
+            agent.append_sample(state, action, reward, next_state, done)
+
+            if len(agent.memory) >= agent.train_start:
+                agent.train_model()
+            score += reward
+            state = next_state
+            if done:
+                agent.update_target_model()
+
+
+
 
 
 
