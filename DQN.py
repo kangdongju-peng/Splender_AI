94c8de486da9a7e0b3c0072bdcb085221a1033e0
kangdongju-peng <kangdongju05@gmail.com>
2021-01-24 23:15:08 +0900

    모델 업데이트 하는부분 잘 보고, 코드 전체적으로 살펴보

--- DQN.py
+++ DQN.py
@@ -15,6 +15,7 @@ EPISODES = 300
 
 class DQNAgent:
     def __init__(self, state_size, action_size):  # 상태와 액션의 크기 받기
+        self.render = False
         self.discount_factor = 0.9  # 감가율 일단 0.9로 설정함
 
         self.epsilon = 1.0  # e-탐욕 정책사용, 탐험률 일단 1.0 설정
@@ -34,8 +35,7 @@ class DQNAgent:
         self.target_model = self.build_agent_model()
         self.model_enemy = self.build_enemy_model()
         self.target_model_enemy = self.build_enemy_model()
-        self.enemy_action = None
-        self.enemy_state = None
+
 
         self.update_target_model()
 
@@ -68,10 +68,11 @@ class DQNAgent:
     def update_target_model_enemy(self):
         self.target_model_enemy.set_weights(self.model_enemy.get_weights())
 
-    def get_action(self, state):
+    def get_action(self, state, enemy_action):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
+            state.append(enemy_action)
             q_value = self.model.predict(state)
             return np.argmax(q_value[0])
 
@@ -91,7 +92,7 @@ class DQNAgent:
         states = np.zeros((self.batch_size, self.state_size))  # 세로열이 batch_size, state_size 가로열 개수인 0으로 도배된 배열 생성
         next_states = np.zeros((self.batch_size, self.state_size))
 
-        actions, rewards = [], []
+        actions, rewards, dones = [], [], []
         for i in range(self.batch_size):
             states[i] = mini_batch[i][0]
             actions.append(mini_batch[i][1])  # 모든 변수에 샘플을 정리
@@ -105,7 +106,7 @@ class DQNAgent:
             if done[i]:
                 target[i][actions[i]] = rewards[i]
             else:
-                target[i][actions[i]] = rewards[i] + self.discount_factor * (np.amax(target_val[i]))
+                target[i][actions[i]] = rewards[i] + self.discount_factor * (np.amax(target_val[i])) #Todo 모델 학습에 적 행동 유추하는 모델도 학습시키기 까먹으면 안되지
         self.model.fit(states, target, batch_size=self.batch_size, epochs=1, verbose=0)
 
 
@@ -114,62 +115,80 @@ if __name__ == "__main__":
     env = Env  # 환경 가저오기!
     state_size = env.state_space  #TODO 환경에 상태의 크기를 가져오는 함수가 필요함
     action_size = env.action_space  #TODO 환경에 액션의 크기를 가져오는 함수가 필여
+
     agent_1 = DQNAgent(state_size, action_size)
     agent_2 = DQNAgent(state_size, action_size)# 이제부터 환경의 함수가 필요한데.,,, 내일 말조 ㅁ해보고 하자
 
     scores_1, episodes_1, scores_2, episodes_2 = [], [], [], []
 
     for e in range(EPISODES):
+        done = False
         score_1 = 0
         score_2 = 0
+
         state_1 = env.reset()#TODO <-- 상태 리셋하는 함수 env에 넣어야함
         state_1 = env.startset()#TODO <-- 시작할때 상태를 나타내는거
+
         state_2 = None # TODO stage의 상대 상태와 자신 상태 바꿔주기
 
 
 
 
-        while not env.end():
+        while not done:
+            if render:
+                GraphicDisplay.show() # Todo render 설정해놓으면 볼 수 있게 하는 함수야
+
             if env.getFirstAgent() == 1:
-                action_1 = agent_1.get_action(state_1)
-                next_state_1, reward_1 = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
-                action_2 = agent_2.get_action(state_2)
-                next_state_2, reward_2 = env.step(action_2)  # 한턴 진행한거야 먼저한친구가 1이면 이렇게
+                action_1 = agent_1.get_action(state_1, agent_1.get_enemy_action(state_2)) # 상대의 액션을 넣어서 한
+                next_state_1, reward_1, done = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
+                action_2 = agent_2.get_action(state_2, agent_2.get_enemy_action(state_1))
+                next_state_2, reward_2, done = env.step(action_2)  # 한턴 진행한거야 먼저한친구가 1이면 이렇게
             if env.getFirstAgent() == 2:
-                action_2 = agent_2.get_action(state_2)
-                next_state_2, reward_2 = env.step(action_2)  # 한턴 진행한거야 먼저한친구가 2이면 이렇게
-                action_1 = agent_1.get_action(state_1)
-                next_state_1, reward_1 = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
+                action_2 = agent_2.get_action(state_2, agent_2.get_enemy_action(state_1))
+                next_state_2, reward_2, done = env.step(action_2)  # 한턴 진행한거야 먼저한친구가 2이면 이렇게
+                action_1 = agent_1.get_action(state_1, agent_1.get_enemy_action(state_2))
+                next_state_1, reward_1, done = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
 
 
 
             next_state_1 = np.reshape(next_state_1, [1, state_size])
-            agent.append_sample(state_1, action_1, reward_1, next_state_1)
+            next_state_2 = np.reshape(next_state_2, [1, state_size])
+
+            agent_1.append_sample(state_1, action_1, reward_1, next_state_1, done)
+            agent_2.append_sample(state_2, action_2, reward_2, next_state_2, done)
 
             if len(agent_1.memory) >= agent.train_start:
                 agent_1.train_model()
-            score_1 += reward_1
-            state_1 = next_state_1
-            agent_1.update_target_model()
-            agent_1.update_target_model_enemy()
-            scores_1.append(score)
-            episodes_1.append(e)  # <-- 이 e가 뭐하는걸까 모르겟음
-            pylab.plot(episodes_1, scores_1, 'b')
-            # pylab.savefig("")    <- 그래프 저장 경로 사실 딱히 필요없음 자소서 쓸때 필요하려나
-
-            next_state_2 = np.reshape(next_state_2, [1, state_size])
-            agent.append_sample(state_2, action_2, reward_2, next_state_2)
-
-            if len(agent_2.memory) >= agent.train_start:
+            #if len(agent_2.memory) >= agent.train_start:    메모리 개수는 똑같아서 그냥 한 함수에 둘다 넣었어.
                 agent_2.train_model()
+
+
+            score_1 += reward_1
             score_2 += reward_2
+
+            state_1 = next_state_1
             state_2 = next_state_2
-            agent_2.update_target_model()
-            agent_2.update_target_model_enemy()
-            scores_2.append(score)
-            episodes_2append(e)  # <-- 이 e가 뭐하는걸까 모르겟음
-            pylab.plot(episodes_2, scores_2, 'b')
-            # pylab.savefig("")    <- 그래프 저장 경로 사실 딱히 필요없음 자소서 쓸때 필요하려나
-            if np.mean(scores_2[-min(10, len(scores_2)):]) > 100:  # 이전 에피소드 점수 평균이 100넘으면 탈출 (학습)
-                # agent.model.save_weights() <-- 이괄호 안에는 학습 데으터를 넣을 경로가 필요한데 아직 못정함
-                sys.exit()
+
+
+
+
+            if done:
+                agent_1.update_target_model()
+                agent_1.update_target_model_enemy()
+                agent_2.update_target_model()
+                agent_2.update_target_model_enemy()
+
+                scores_2.append(score)
+                episodes_2append(e)  # <-- 이 e가 뭐하는걸까 모르겟음
+                pylab.plot(episodes_2, scores_2, 'b')
+
+                scores_1.append(score)
+                episodes_1.append(e)  # <-- 이 e가 뭐하는걸까 모르겟음
+                pylab.plot(episodes_1, scores_1, 'b')
+
+                #pylab.savefig("")    <- 그래프 저장 경로 사실 딱히 필요없음 자소서 쓸때 필요하려나
+                if np.mean(scores_2[-min(10, len(scores_2)):]) > 100:  # 이전 에피소드 점수 평균이 100넘으면 탈출 (학습)
+                    # agent.model.save_weights() <-- 이괄호 안에는 학습 데으터를 넣을 경로가 필요한데 아직 못정함
+                    sys.exit()
+                    #아래있는거 그래프 + 가중치 동기화
+                    # 그래프 + 가중치 기화
