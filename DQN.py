b71a5e13391e85584fb25c4c24c0bab936d433b7
Jaehwan-cloud <jaehwanl097@gmail.com>
2021-01-30 17:22:35 +0900

    거의 완성

--- DQN.py
+++ DQN.py
@@ -15,82 +15,81 @@ EPISODES = 300
 
 class DQNAgent:
     def __init__(self, state_size, action_size):  # 상태와 액션의 크기 받기
-        self.render = False # 이 친구는 바로 바로 바로 바로 그래픽을 보이게 말지 결정하는 친구
-        self.load_model = False # 이친구는 기존에 가중치를 가져올것인가를 결정하는 친
+        self.render = False  # 이 친구는 바로 바로 바로 바로 그래픽을 보이게 말지 결정하는 친구
+        self.load_model = False  # 이친구는 기존에 가중치를 가져올것인가를 결정하는 친
 
         self.discount_factor = 0.9  # 감가율 일단 0.9로 설정함
 
         self.epsilon = 1.0  # e-탐욕 정책사용, 탐험률 일단 1.0 설정
         self.epsilon_min = 0.01  # 탐험률의 최소 값 정의
         self.epsilon_decay = 0.999  # 탐험률의 감소값 일단 0.999로 설정
-        self.train_start = 1000 # 학습을 시작하는 판수
-        self.learning_rate = 0.001 # 인공신경망에 사용되는 학습
+        self.train_start = 1000  # 학습을 시작하는 판수
+        self.learning_rate = 0.001  # 인공신경망에 사용되는 학습
 
-        self.state_size = state_size # 인공지능망의 인풋 개수, 상태의 갯수랍니
-        self.action_size = action_size다 # 인공신경망에 아웃풋 개수! TODO 액션리스트중에서 할 수 없는것 가리
+        self.state_size = state_size  # 인공지능망의 인풋 개수, 상태의 갯수랍니
+        self.action_size = action_size다  # 인공신경망에 아웃풋 개수! TODO 액션리스트중에서 할 수 없는것 가리
 
         self.batch_size = 64  # 한번에 학습을 위해 가져올 샘플 수, 조정 필
 
         self.memory = deque(maxlen=2000)  # 메모리 최대크기 2000으로 설정함
 
-        self.model = self.build_agent_model() # 행동 결정 인공신경망
-        self.target_model = self.build_agent_model() #탈겟 인공신경망
-        self.model_enemy = self.build_enemy_model() # 상대 행동 예측 인공신경망
-        self.target_model_enemy = self.build_enemy_model() # 상대 행동 예측 탈겟 인공신경망
+        self.model = self.build_agent_model()  # 행동 결정 인공신경망
+        self.target_model = self.build_agent_model()  # 탈겟 인공신경망
+        self.model_enemy = self.build_enemy_model()  # 상대 행동 예측 인공신경망
+        self.target_model_enemy = self.build_enemy_model()  # 상대 행동 예측 탈겟 인공신경망
 
-
-        self.update_target_model() # 가중치 동기화 함수
-        self.update_target_model_enemy() # 가중치 동기화 합수
+        self.update_target_model()  # 가중치 동기화 함수
+        self.update_target_model_enemy()  # 가중치 동기화 합수
 
     #       if self.load_model(): TODO 가중치 불러오긴가 뭔가 그거 해놓기 주소 만들
     #          self.model.load_weights("./save_model/cartpole_dqn_trained.h5") ## 일단 책에 중요해 보여서 썼는데, 뭔 코든지 해석좀
 
     def build_agent_model(self):  # 행동을 결정하는 인공신경망
         model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size + 1, activation='relu', kernel_initializer='he_uniform')) # 여기서는 상대의 예상행동도 인수로 받기때문에 state+1
+        model.add(Dense(24, input_dim=self.state_size + 1, activation='relu',
+                        kernel_initializer='he_uniform'))  # 여기서는 상대의 예상행동도 인수로 받기때문에 state+1
         model.add(Dense(24, activation='relu', kernel_initializer='he_uniform'))
         model.add(Dense(self.action_size, activation='linear', kernel_initializer='he_uniform'))
         model.summary()
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
 
-
-    def build_enemy_model(self): # 에네미 행동을 추측하는 인공신경
+    def build_enemy_model(self):  # 에네미 행동을 추측하는 인공신경
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu', kernel_initializer='he_uniform'))
         model.add(Dense(24, activation='relu', kernel_initializer='he_uniform'))
         model.add(Dense(self.action_size, activation='linear', kernel_initializer='he_uniform'))
         model.summary()
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        # pass
         return model
 
-
-    def update_target_model(self): # 가중치 동기화
+    def update_target_model(self):  # 가중치 동기화
         self.target_model.set_weights(self.model.get_weights())
 
-    def update_target_model_enemy(self): # 가중치 동기화
+    def update_target_model_enemy(self):  # 가중치 동기화
         self.target_model_enemy.set_weights(self.model_enemy.get_weights())
 
-    def get_action(self, state, enemy_action): # 인공신경망을 이용해서 상태와 상대 예상행동을 받고 행동을 리턴
+    def get_action(self, state, enemy_action):  # 인공신경망을 이용해서 상태와 상대 예상행동을 받고 행동을 리턴
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size) # 탐험률
+            return random.randrange(self.action_size)  # 탐험률
         else:
-            state.append(enemy_action) # 상태에 상대방 예상 행동도 추가해서 인공신경망에 넣기
+            state.append(enemy_action)  # 상태에 상대방 예상 행동도 추가해서 인공신경망에 넣기
             q_value = self.model.predict(state)
             return np.argmax(q_value[0])
 
-    def get_enemy_action(self, enemy_state): #에네미의 예상행동을 반환하는 함수, 탐험 없고 상대 행동받는것 없음
+    def get_enemy_action(self, enemy_state):  # 에네미의 예상행동을 반환하는 함수, 탐험 없고 상대 행동받는것 없음
         q_value = self.model.predict(enemy_state)
         return np.argmax(q_value[0])
 
-    def append_sample(self, state, action, reward, next_state, done):# 학습샘플을 메모리에 저장하는 함수
+    def append_sample(self, state, action, reward, next_state, done):  # 학습샘플을 메모리에 저장하는 함수
         self.memory.append((state, action, reward, next_state, done))
 
     def train_model(self):  # 학습시키기
         if self.epsilon > self.epsilon_min:
-            self.epsilon = self.epsilon * self.epsilon_decay # 입실론 조정치를 조금씩 줄여감
+            self.epsilon = self.epsilon * self.epsilon_decay  # 입실론 조정치를 조금씩 줄여감
 
-        mini_batch = random.sample(self.memory, self.batch_size) # 메모리에서 랜덤으로 배치 사이즈많큼 샘플을 뽑아옴
+        mini_batch = random.sample(self.memory, self.batch_size)  # 메모리에서 랜덤으로 배치 사이즈많큼 샘플을 뽑아옴
 
         states = np.zeros((self.batch_size, self.state_size))  # 세로열이 batch_size, state_size 가로열 개수인 0으로 도배된 배열 생성
         next_states = np.zeros((self.batch_size, self.state_size))
@@ -105,21 +104,23 @@ class DQNAgent:
 
         target = self.model.predict(states)
         target_val = self.target_model.predict(next_states)
+
         for i in range(self.batch_size):
             if done[i]:
-                target[i][actions[i]] = rewards[i]      #벨만기대방정식을 이용해서 인공신경망 업데이
+                target[i][actions[i]] = rewards[i]  # 벨만기대방정식을 이용해서 인공신경망 업데이
             else:
-                target[i][actions[i]] = rewards[i] + self.discount_factor * (np.amax(target_val[i])) #Todo 모델 학습에 적 행동 유추하는 모델도 학습시키기 까먹으면 안되지
+                target[i][actions[i]] = rewards[i] + self.discount_factor * (
+                    np.amax(target_val[i]))  # Todo 모델 학습에 적 행동 유추하는 모델도 학습시키기 까먹으면 안되지
         self.model.fit(states, target, batch_size=self.batch_size, epochs=1, verbose=0)
 
-    def train_model_enemy(self, memory): # 적의 메모리를 인수로 받아 학습
-        mini_batch = random.sample(memory, self.batch_size) # 상대 메모리에서 샘플 추출
+    def train_model_enemy(self, memory):  # 적의 메모리를 인수로 받아 학습
+        mini_batch = random.sample(memory, self.batch_size)  # 상대 메모리에서 샘플 추출
 
         states = np.zeros((self.batch_size, self.state_size))  # 세로열이 batch_size, state_size 가로열 개수인 0으로 도배된 배열 생성
         next_states = np.zeros((self.batch_size, self.state_size))
 
         actions, rewards, dones = [], [], []
-        for i in range(self.batch_size):#이건바뀔필요가 없어 그냥 상수
+        for i in range(self.batch_size):  # 이건바뀔필요가 없어 그냥 상수
             states[i] = mini_batch[i][0]
             actions.append(mini_batch[i][1])  # 모든 변수에 샘플을 정리
             rewards.append(mini_batch[i][2])
@@ -139,11 +140,11 @@ class DQNAgent:
 if __name__ == "__main__":
 
     env = Env  # 환경 가저오기!
-    state_size = env.state_space  #TODO 환경에 상태의 크기를 가져오는 함수가 필요함
-    action_size = env.action_space  #TODO 환경에 액션의 크기를 가져오는 함수가 필여
+    state_size = env.state_space  # TODO 환경에 상태의 크기를 가져오는 함수가 필요함
+    action_size = env.action_space  # TODO 환경에 액션의 크기를 가져오는 함수가 필여
 
     agent_1 = DQNAgent(state_size, action_size)
-    agent_2 = DQNAgent(state_size, action_size) # 이제부터 환경의 함수가 필요한데.,,, 내일 말조 ㅁ해보고 하자
+    agent_2 = DQNAgent(state_size, action_size)  # 이제부터 환경의 함수가 필요한데.,,, 내일 말조 ㅁ해보고 하자
 
     scores_1, episodes_1, scores_2, episodes_2 = [], [], [], []
 
@@ -152,20 +153,17 @@ if __name__ == "__main__":
         score_1 = 0
         score_2 = 0
 
-        state_1 = env.reset()#TODO <-- 상태 리셋하는 함수 env에 넣어야함
-        state_1 = env.startset()#TODO <-- 시작할때 상태를 나타내는거
+        state_1 = env.reset()  # TODO <-- 상태 리셋하는 함수 env에 넣어야함
+        state_1 = env.startset()  # TODO <-- 시작할때 상태를 나타내는거
 
-        state_2 = None # TODO stage의 상대 상태와 자신 상태 바꿔주기
-
-
-
+        state_2 = None  # TODO stage의 상대 상태와 자신 상태 바꿔주기
 
         while not done:
             if render:
-                GraphicDisplay.show() # Todo render 설정해놓으면 볼 수 있게 하는 함수야
+                GraphicDisplay.show()  # Todo render 설정해놓으면 볼 수 있게 하는 함수야
 
             if env.getFirstAgent() == 1:
-                action_1 = agent_1.get_action(state_1, agent_1.get_enemy_action(state_2)) # 상대의 액션을 넣어서 한
+                action_1 = agent_1.get_action(state_1, agent_1.get_enemy_action(state_2))  # 상대의 액션을 넣어서 한
                 next_state_1, reward_1, done = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
                 action_2 = agent_2.get_action(state_2, agent_2.get_enemy_action(state_1))
                 next_state_2, reward_2, done = env.step(action_2)  # 한턴 진행한거야 먼저한친구가 1이면 이렇게
@@ -175,7 +173,11 @@ if __name__ == "__main__":
                 action_1 = agent_1.get_action(state_1, agent_1.get_enemy_action(state_2))
                 next_state_1, reward_1, done = env.step(action_1)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
 
-
+        while not done:
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)  # TODO env에 한 액션을 받고 한 턴을 플레이 하는 함수르 만들기
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.append_sample(state, action, reward, next_state, done)
 
             next_state_1 = np.reshape(next_state_1, [1, state_size])
             next_state_2 = np.reshape(next_state_2, [1, state_size])
@@ -186,20 +188,15 @@ if __name__ == "__main__":
             if len(agent_1.memory) >= agent.train_start:
                 agent_1.train_model()
                 agent_1.train_model_enemy(agent_2.memory)
-            #if len(agent_2.memory) >= agent.train_start:    메모리 개수는 똑같아서 그냥 한 함수에 둘다 넣었어.
+                # if len(agent_2.memory) >= agent.train_start:    메모리 개수는 똑같아서 그냥 한 함수에 둘다 넣었어.
                 agent_2.train_model()
                 agent_2.train_model_enemy(agent_1.memory)
-
-
 
             score_1 += reward_1
             score_2 += reward_2
 
             state_1 = next_state_1
             state_2 = next_state_2
-
-
-
 
             if done:
                 agent_1.update_target_model()
@@ -215,9 +212,9 @@ if __name__ == "__main__":
                 episodes_1.append(e)  # <-- 이 e가 뭐하는걸까 모르겟음
                 pylab.plot(episodes_1, scores_1, 'b')
 
-                #pylab.savefig("")    <- 그래프 저장 경로 사실 딱히 필요없음 자소서 쓸때 필요하려나
+                # pylab.savefig("")    <- 그래프 저장 경로 사실 딱히 필요없음 자소서 쓸때 필요하려나
                 if np.mean(scores_2[-min(10, len(scores_2)):]) > 100:  # 이전 에피소드 점수 평균이 100넘으면 탈출 (학습)
                     # agent.model.save_weights() <-- 이괄호 안에는 학습 데으터를 넣을 경로가 필요한데 아직 못정함
                     sys.exit()
-                    #아래있는거 그래프 + 가중치 동기화
+                    # 아래있는거 그래프 + 가중치 동기화
                     # 그래프 + 가중치 기화
--- enviornment.py
+++ enviornment.py
@@ -1,101 +1,22 @@
 from random import *
+from functools import reduce
 
+
+# 이곳에 있는 모든 토큰은 white blue green red black gold 순으로
+# 0 1 2 3 4 5 의 번호를 가짐을 알립니다
 
 class Env:
+
     def __init__(self):
-        self.cards_game = None
-        self.cards = (
-            ('k011110',
-             'k012110',
-             'k022010',
-             'k000131',
-             'k000210',
-             'k020200',
-             'k000300',
-             'k104000',
-             'u010111',
-             'u010121',
-             'u010220',
-             'u001310',
-             'u010002',
-             'u000202',
-             'u000003',
-             'u100040',
-             'w001111',
-             'w001211',
-             'w002201',
-             'w031001',
-             'w000021',
-             'w002002',
-             'w003000',
-             'w100400',
-             'g011011',
-             'g011012',
-             'g001022',
-             'g013100',
-             'g021000',
-             'g002020',
-             'g000030',
-             'g100004',
-             'r011101',
-             'r021101',
-             'r021101',
-             'r020102',
-             'r010013',
-             'r002100',
-             'r020020',
-             'r030000',),
-            ('k132200',
-             'k130302',
-             'k201420',
-             'k200530',
-             'k205000',
-             'k300006',
-             'u102230',
-             'u102303',
-             'u253000',
-             'u220014',
-             'u205000',
-             'u306000',
-             'w100322',
-             'w123030',
-             'w200142',
-             'w200053',
-             'w200050',
-             'w360000',
-             'g130230',
-             'g123002',
-             'g242001',
-             'g205300',
-             'g200500',
-             'g300600',
-             'r120023',
-             'r103023',
-             'r214200',
-             'r230005',
-             'r200005',
-             'r300060',),
-            ('k333530',
-             'k433530',
-             'k400070',
-             'k500073',
-             'u330335',
-             'u470000',
-             'u463003',
-             'u573000',
-             'w303353',
-             'w400007',
-             'w430036',
-             'w530007',
-             'g353033',
-             'g407000',
-             'g436300',
-             'g507300',
-             'r335303',
-             'r400700',
-             'r400700',
-             'r500730',),
-        )
+        self.Token = {'w': 0,
+                      'b': 1,
+                      'g': 2,
+                      'r': 3,
+                      'k': 4}
+        self.done = False
+
+        self.turn = 0  # 턴 0번플레이어, 1번플레이어로 나뉨
+
         self.lords = ('340004',
                       '344000',
                       '333300',
@@ -105,25 +26,120 @@ class Env:
                       '330033',
                       '303330',
                       '300440',
-                      '333003',)
-        self.lord_now = None
-        self.tokens = None
-        self.setting()
-        return
+                      '333003',)  # 귀족 종류
+        self.cards = (('k011110',
+                       'k012110',
+                       'k022010',
+                       'k000131',
+                       'k000210',
+                       'k020200',
+                       'k000300',
+                       'k104000',
+                       'u010111',
+                       'u010121',
+                       'u010220',
+                       'u001310',
+                       'u010002',
+                       'u000202',
+                       'u000003',
+                       'u100040',
+                       'w001111',
+                       'w001211',
+                       'w002201',
+                       'w031001',
+                       'w000021',
+                       'w002002',
+                       'w003000',
+                       'w100400',
+                       'g011011',
+                       'g011012',
+                       'g001022',
+                       'g013100',
+                       'g021000',
+                       'g002020',
+                       'g000030',
+                       'g100004',
+                       'r011101',
+                       'r021101',
+                       'r021101',
+                       'r020102',
+                       'r010013',
+                       'r002100',
+                       'r020020',
+                       'r030000',), ('k132200',
+                                     'k130302',
+                                     'k201420',
+                                     'k200530',
+                                     'k205000',
+                                     'k300006',
+                                     'u102230',
+                                     'u102303',
+                                     'u253000',
+                                     'u220014',
+                                     'u205000',
+                                     'u306000',
+                                     'w100322',
+                                     'w123030',
+                                     'w200142',
+                                     'w200053',
+                                     'w200050',
+                                     'w360000',
+                                     'g130230',
+                                     'g123002',
+                                     'g242001',
+                                     'g205300',
+                                     'g200500',
+                                     'g300600',
+                                     'r120023',
+                                     'r103023',
+                                     'r214200',
+                                     'r230005',
+                                     'r200005',
+                                     'r300060',), ('k333530',
+                                                   'k433530',
+                                                   'k400070',
+                                                   'k500073',
+                                                   'u330335',
+                                                   'u470000',
+                                                   'u463003',
+                                                   'u573000',
+                                                   'w303353',
+                                                   'w400007',
+                                                   'w430036',
+                                                   'w530007',
+                                                   'g353033',
+                                                   'g407000',
+                                                   'g436300',
+                                                   'g507300',
+                                                   'r335303',
+                                                   'r400700',
+                                                   'r400700',
+                                                   'r500730',),)  # 카드 종류
 
-    # 게임 세팅
-    def setting(self):
+        self.cards_game = [[], [], []]  # 순서 없이 섞인 카드들
+
+        self.cards_now = [[], [], []]  # 현제 나와있는 1 2 3티어 카드
+        self.lord_now = None  # 본 판에 나와있는 귀족들
         self.tokens = [4, 4, 4, 4, 4, 5]
+
+        self.my_score = [0, 0]
+        self.my_token = [[0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0]]  # 내 토큰
+        self.my_card = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]]  # 내 카드
+        self.my_kept_card = []  # 내가 찜한 카드
+        self.my_lord = [[], []]
+
         for i in range(0, 3):
-            shuffle(self.cards[i])
+            self.cards_game[i] = list(self.cards[i])
+        for i in range(0, 3):
+            shuffle(self.cards_game[i])
         for i in range(0, 3):
             self.cards_game[i] = list(self.cards[i])
             shuffle(self.cards_game[i])
+            self.cards_now[i] = [self.cards_game[i][:3]]
+            del self.cards_game[i][:3]
         self.lord_now = sample(self.lords, 3)
-        return
-
-    # TODO 상태 크기를 내보내는 함수
-    def state_space(self):
         return
 
     # TODO 상태를 초기화하는 함수
@@ -134,28 +150,102 @@ class Env:
     def start_set(self):
         return
 
-    # TODO 토큰을 가져오는 함수
-    def collect(self):
+    # 토큰을 가져오는 함수
+    # collect(가져올 토큰:int, 가져올 토큰/필수아님, 가져올 토큰/필수아님)
+    def collect(self, *to_collect):
+        if len(to_collect) == 1:
+            # check collectable
+            if self.tokens[to_collect[0]] >= 4:
+                self.tokens[self.turn][to_collect[0]] -= 2
+                self.my_token[to_collect[0]] += 2
+            else:
+                return False  # I hope u 2 make AI which can prejudge this thing..or give it as state?
+        elif len(to_collect) == 3:
+            for jem in to_collect:
+                if jem >= 5:
+                    return False
+                if not self.tokens[jem] == 0:
+                    continue
+                else:
+                    return False
+            for jem in to_collect:
+                self.tokens[jem] -= 1
+                self.my_token[self.turn][jem] += 1
         return
 
-    # TODO 킵하는 함수
-    def keep(self):
+    # 킵하는 함수
+    # ** card 는 [n, m]형태의 리스트로 n = 티어 m = 번째 / 5번째 = 뒷면
+    def keep(self, card):
+        if len(self.my_kept_card) >= 3:
+            return False
+        if self.tokens[5] > 0:
+            self.tokens[5] -= 1
+            self.my_token[self.turn][5] += 1
+        if card[1] == 5:
+            self.my_kept_card.append(self.cards_game[card[0] - 1][0])
+            del self.cards_game[card[0] - 1][0]
+        else:
+            self.my_kept_card.append(self.cards_now[card[0] - 1][card[1] - 1])
+            del self.cards_now[card[0] - 1][card[1] - 1]
+            self.cards_now[card[0]][card[1] - 1] = self.cards_game[card[0] - 1][0]
+            del self.cards_game[card[0] - 1][card[1] - 1]
         return
 
-    # TODO 구매하는 함수
-    def buy(self):
+    # 구매하는 함수
+    # ** card 는 [n, m]형태의 리스트로 n = 티어 m = 번째 / 4티어 = 킵 카드
+    def buy(self, card):
+        if card[0] == 4:
+            if len(self.my_kept_card[self.turn]) == 0:
+                return False
+            else:
+                self.price(self.my_kept_card[self.turn][card[1]])
+        else:
+            self.price(self.cards_now[card[0]][card[1]])
+
         return
 
-    # TODO 실행가능한 액션의 개수를 반환하는 함수
-    def action_space(self):
+    # 가격 계산 함수
+    # buy 에 포함시켜도 되는데 헷갈려서 그냥 함
+    def price(self, card_str):
+        ori_token = self.my_token[self.turn]
+        for i in range(0, 5):
+            price = int(card_str[i + 2]) - int(self.my_card[self.turn][i])
+            if price <= self.my_token[self.turn][i]:
+                self.my_token[self.turn][i] -= price
+            elif price <= self.my_token[self.turn][i] + self.my_token[self.turn][5]:
+                self.my_token[self.turn][5] -= price - self.my_token[self.turn][i]
+                self.my_token[self.turn][i] = 0
+            else:
+                self.my_token = ori_token
+                return False
+            self.my_card[self.turn][self.Token[card_str[0]]] += 1
+            self.my_score[self.turn] += card_str[1]
+        return True
+
+    # TODO 토큰 퉤
+    def spit(self):
         return
 
-    # TODO 15점이 넘고 턴이 종료되었는지 확인하는 함수
-    def end(self):
-        return
-
-    # TODO 액션을 받고 한 턴을 플레이하는 함수
+    # TODO 말그대로 step
+    # TODO 액션을 어케받는게좋을까
     def step(self):
+        if reduce(lambda x, y: x + y, self.my_token[self.turn]) > 10:
+            self.spit()
+        for score in self.my_score:
+            if score > 15 and self.turn == 1:
+                done = True
+        for lord in self.lord_now:
+            lord_list = list(lord)
+            del lord_list[0]
+            if self.my_card == lord_list:
+                self.my_lord[self.turn].append(lord)
+                self.lord_now.remove(lord)
+                self.my_score[self.turn] += int(lord[0])
+        return self.next_state, self.reward
+
+
+class Player:
+    def __init__(self):
         return
 
 
@@ -163,3 +253,9 @@ class GraphicDisplay:
     def __init__(self):
         # 안에 만들어
         return
+
+
+if __name__ == "__main__":
+    env = Env()
+    for ch in env.price(env.cards[0][0]):
+        print(ch)
